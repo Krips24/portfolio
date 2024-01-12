87f6f097398110ed0bc895345173c6d9c78d31ee
krapansh <krapansh24@gmail.com>
2024-01-12 11:38:03 +0530

    update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,10 @@ function Navbar() {
     <div>
       <header className="text-gray-400 bg-gray-900 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+          <Link
+            href={"/"}
+            className="flex title-font font-medium items-center text-white mb-4 md:mb-0"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -20,7 +23,7 @@ function Navbar() {
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
             <span className="ml-3 text-xl">Krapansh</span>
-          </a>
+          </Link>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             <Link href={"/"} className="mr-5 hover:text-white">
               {" "}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
-import React from "react";
-
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <section className="text-gray-400 bg-gray-900 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+              Hi, I'm Krapansh
+              <br className="hidden lg:inline-block" />
+              Welcome to my Portfolio
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Welcome to my Portfolio profile! I'm an Electronics student with a
+              keen interest in exploring the fascinating realms of Full Stack
+              Web Development and Data Structures and Algorithms (DSA).
+            </p>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="/home.png"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Home;

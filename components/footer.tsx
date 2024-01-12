47413cc97efe340f24e81f89810637351d332089
krapansh <krapansh24@gmail.com>
2024-01-12 21:14:45 +0530

    updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="text-gray-600 body-font">
         <div className="container px-5 py-8 mx-auto flex items-center justify-center sm:flex-row flex-col">
           <span className="inline-flex sm:mt-0 mt-4 items-center space-x-3">
-            <a className="text-gray-500">
+            <a href="https://github.com/Krips24" className="text-gray-500">
               <svg
                 fill="currentColor"
                 stroke-linecap="round"
@@ -18,7 +18,10 @@ const Footer = () => {
                 <path d="M12,2.2467A10.00042,10.00042,0,0,0,8.83752,21.73419c.5.08752.6875-.21247.6875-.475,0-.23749-.01251-1.025-.01251-1.86249C7,19.85919,6.35,18.78423,6.15,18.22173A3.636,3.636,0,0,0,5.125,16.8092c-.35-.1875-.85-.65-.01251-.66248A2.00117,2.00117,0,0,1,6.65,17.17169a2.13742,2.13742,0,0,0,2.91248.825A2.10376,2.10376,0,0,1,10.2,16.65923c-2.225-.25-4.55-1.11254-4.55-4.9375a3.89187,3.89187,0,0,1,1.025-2.6875,3.59373,3.59373,0,0,1,.1-2.65s.83747-.26251,2.75,1.025a9.42747,9.42747,0,0,1,5,0c1.91248-1.3,2.75-1.025,2.75-1.025a3.59323,3.59323,0,0,1,.1,2.65,3.869,3.869,0,0,1,1.025,2.6875c0,3.83747-2.33752,4.6875-4.5625,4.9375a2.36814,2.36814,0,0,1,.675,1.85c0,1.33752-.01251,2.41248-.01251,2.75,0,.26251.1875.575.6875.475A10.0053,10.0053,0,0,0,12,2.2467ZF"></path>
               </svg>
             </a>
-            <a className="text-gray-500">
+            <a
+              href="https://twitter.com/KrapanshS98944"
+              className="text-gray-500"
+            >
               <svg
                 fill="currentColor"
                 stroke-linecap="round"
@@ -30,7 +33,10 @@ const Footer = () => {
                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
               </svg>
             </a>
-            <a className="text-gray-500">
+            <a
+              href="https://www.instagram.com/_rogerkrips24/"
+              className="text-gray-500"
+            >
               <svg
                 fill="none"
                 stroke="currentColor"
@@ -44,7 +50,10 @@ const Footer = () => {
                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
               </svg>
             </a>
-            <a className="text-gray-500">
+            <a
+              href="https://www.linkedin.com/in/krapanshshrivastava/"
+              className="text-gray-500"
+            >
               <svg
                 fill="currentColor"
                 stroke="currentColor"
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,15 @@
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="bg-image">
+      <Navbar />
+      
+      <Footer />
+    </div>
+  );
 };
 
 export default About;
